b9700687e7b3a05251d93f4cfec4dbc01f79f453
hassan <hassanelaraby46@gmail.com>
2026-06-20 10:29:12 +0300

    تحديث صفحة الخدمات الإدارية

--- medical/settings.py
+++ medical/settings.py
@@ -116,13 +116,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
 # هذا المسار يحدد أين ستذهب ملفات الـ CSS والصور بعد تجميعها
-STATIC_ROOT = '/home/hassan22/hematemsis-unit/static'
+STATIC_ROOT = '/home/hassan22/hematemsis-unit/staticfiles'
 
 # تأكد أن هذا السطر موجود أيضاً
 STATIC_URL = '/static/'
